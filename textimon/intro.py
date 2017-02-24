9e16b456e3465ae03ef1558d203a7c2d1f0f6342
njdaniel <nicholasjdaniel@gmail.com>
2017-02-24 09:24:16 -0500

    add kv file

--- textimon/intro.py
+++ textimon/intro.py
@@ -5,11 +5,11 @@ kivy.require('1.9.0')
 from kivy.app import App
 from kivy.uix.button import Label
 
-class Intro(App):
+class IntroApp(App):
 
     def build(self):
-        return Label(text='Test This')
+        return Label()
 
 if __name__=='__main__':
-    intro = Intro()
+    intro = IntroApp()
     intro.run()
